5ac3bb66b2651e99c23c7688e238342de0adb354
fatma1604 <fatmanurkamis34@gmail.com>
2024-10-14 15:14:25 +0300

    navigatora sayfa ekleme

---
+++ istegram_clone/lib/screen/add_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddScreen extends StatefulWidget {
+  const AddScreen({super.key});
+
+  @override
+  State<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  late PageController pageController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

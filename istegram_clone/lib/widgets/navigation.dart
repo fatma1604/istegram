a38c97d6ac37723ee175651a15e79f9da5f6b839
fatma1604 <fatmanurkamis34@gmail.com>
2024-10-14 15:08:24 +0300

    login-register

---
+++ istegram_clone/lib/widgets/navigation.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class Navigations_Screen extends StatefulWidget {
+  const Navigations_Screen({super.key});
+
+  @override
+  State<Navigations_Screen> createState() => _Navigations_ScreenState();
+}
+
+class _Navigations_ScreenState extends State<Navigations_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
